88fd727d68b65b6410dee5fdb2c673539b1bebb4
andrewyoo <yooandrew2004@gmail.com>
2026-07-21 23:00:26 -0400

    new edits

--- src/consts.ts
+++ src/consts.ts
@@ -9,10 +9,8 @@ export const AUTHOR = {
   email: "yooandrew2004@gmail.com",
   github: "https://github.com/yoohyoungjoon",
   linkedin: "https://www.linkedin.com/in/hyoungjoon-yoo-61a751203/",
-  // These point to PDFs you drop into the public/ folder. The links will 404
-  // until those files exist. Rename here if you name the files differently.
-  cv: "/Resume260604.pdf", // shown in the footer
-  transcript: "/ColumbiaTranscriptFinal.pdf", // linked from the About page
+  cv: "/Resume260604.pdf", 
+  transcript: "/ColumbiaTranscriptFinal.pdf",
 };
 
 // Top-nav sections, in order. Each maps to a folder in src/content/ and a
